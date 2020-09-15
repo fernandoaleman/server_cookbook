c8d99c58f0c2eb02cfdcc6291261ccef300e5a8b
Fernando Alemán <fernandoaleman@mac.com>
2020-09-15 15:31:05 -0500

    Install epel package and repo

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -4,5 +4,6 @@ describe 'server::default' do
   platform 'centos'
 
   context 'with default attributes' do
+    it { is_expected.to install_package('epel-release') }
   end
 end
--- test/integration/default/controls/default_spec.rb
+++ test/integration/default/controls/default_spec.rb
@@ -1,4 +1,13 @@
-control 'default' do
-  title 'Ensure default test runs successfully.'
+control 'epel' do
+  title 'Ensure epel package and repo are installed.'
   impact 1.0
+
+  describe package('epel-release') do
+    it { should be_installed }
+  end
+
+  describe yum.repo('epel') do
+    it { should exist }
+    it { should be_enabled }
+  end
 end
